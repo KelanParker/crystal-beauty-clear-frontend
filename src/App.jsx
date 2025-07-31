8037e54966473a802ddb579ce02cf13e6660729f
KelanParker <sa25610215@my.sliit.lk>
2025-07-31 12:24:59 +0530

    login connected with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AdminPage from './pages/adminPage';
 import LoginPage from './pages/loginPage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes path="/*">
+      <Toaster position="top-center" />
+      <Routes>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/admin/*" element={<AdminPage/>}/>
         <Route path="/" element={<h1>Home Page</h1>}/>
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,22 +1,60 @@
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
-  return (
-    <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex">
-        <div className= " w-[50%] h-full">
 
-        </div>
-        <div className= " w-[50%] h-full flex justify-center items-center">
-            <div className= "w-[450px] h-[600px] backdrop-blur-lg shadow-lg rounded-2xl flex flex-col justify-center items-center">
-                <input className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" type="email" placeholder="Email" />
-                <input className="w-[400px] h-[50px] border border-white rounded-2xl text-center " type="password" placeholder="Password" />
-                <button>
-                    <div className="w-[400px] h-[50px] bg-green-400 text-white flex justify-center items-center rounded-2xl m-[5px] hover:bg-green-500 transition duration-300 cursor-pointer">
-                        Login
-                    </div>
-                </button>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        axios.post(import.meta.env.VITE_BACKEND_URL + '/users/login', { email, password }).then((response) => {
+            console.log("Login successful:", response.data);
+            toast.success("Login successful!");
+            localStorage.setItem('token', response.data.token);
+
+            const user = response.data.user;
+            if (user.role === 'admin') {
+                navigate('/admin');
+            }
+            else {
+                navigate('/');
+            }
+
+        }).catch((error) => {
+            console.error("Login failed:", error);
+            toast.error(<error className="response data message">{error.response.data.message}</error> || "Login failed. Please check your credentials.");
+            // Handle login failure, e.g., show error message
+        });
+    }
+
+    return (
+        <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex">
+            <div className="w-[50%] h-full">
             </div>
-
+            <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[450px] h-[600px] backdrop-blur-lg shadow-lg rounded-2xl flex flex-col justify-center items-center">
+                    <input onChange = {(e) => setEmail(e.target.value)}
+                        className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" 
+                        type="email" 
+                        placeholder="Email" 
+                    />
+                    <input onChange = {(e) => setPassword(e.target.value)}
+                        className="w-[400px] h-[50px] border border-white rounded-2xl text-center" 
+                        type="password" 
+                        placeholder="Password" 
+                    />
+                    <button onClick={handleLogin} className="w-[400px] h-[50px] bg-green-400 text-white flex justify-center items-center rounded-2xl m-[5px] hover:bg-green-500 transition duration-300 cursor-pointer">
+                        Login
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
